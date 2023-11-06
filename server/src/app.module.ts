6342e6e683286b43229918fa48e6532e5903262d
ohbyul <b.oh@urbanisom.com>
2023-11-06 13:52:58 +0900

    6st

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -13,28 +13,28 @@ import { COMMON } from './entitys/common/common.model';
 import { CommonModule } from './common/common.module';
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: ['.env', '.code'],
-    }),
-    SequelizeModule.forRoot({
-      dialect: 'mysql',
-      host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.DATABASE_PORT),
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      timezone: "+09:00",
-      synchronize: true,
-      pool: {
-        acquire: 10000,
-        idle: 1000,
-        max: 10,
-        min: 5
-      },
-      logging: process.env.DATABASE_LOGGING == null ? false : process.env.DATABASE_LOGGING === 'true',
-      models: [COMMON],
-    }),
-    CommonModule,
+    // ConfigModule.forRoot({
+    //   envFilePath: ['.env', '.code'],
+    // }),
+    // SequelizeModule.forRoot({
+    //   dialect: 'mysql',
+    //   host: process.env.DATABASE_HOST,
+    //   port: parseInt(process.env.DATABASE_PORT),
+    //   username: process.env.DATABASE_USER,
+    //   password: process.env.DATABASE_PASSWORD,
+    //   database: process.env.DATABASE_NAME,
+    //   timezone: "+09:00",
+    //   synchronize: true,
+    //   pool: {
+    //     acquire: 10000,
+    //     idle: 1000,
+    //     max: 10,
+    //     min: 5
+    //   },
+    //   logging: process.env.DATABASE_LOGGING == null ? false : process.env.DATABASE_LOGGING === 'true',
+    //   models: [COMMON],
+    // }),
+    // CommonModule,
   ],
   controllers: [AppController],
   providers: [AppService,
